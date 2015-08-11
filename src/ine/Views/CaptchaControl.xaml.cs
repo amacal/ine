8ba7b656a498820f4e98f32afe54cfe30dd37425
Adrian Macal <adma@amacal.pl>
2015-08-11 13:38:36 +0200

    Support enabling and disabling captcha buttons.

--- src/ine/Views/CaptchaControl.xaml.cs
+++ src/ine/Views/CaptchaControl.xaml.cs
@@ -18,57 +18,87 @@ namespace ine.Views
 
         public Task<string> Solve(Captcha captcha)
         {
+            Action dispose = null, initialize = null;
             EventHandler timerHandler = null;
             RoutedEventHandler solveHandler = null;
             RoutedEventHandler reloadHandler = null;
+            RoutedEventHandler cancelHandler = null;
 
             TaskCompletionSource<string> completion = new TaskCompletionSource<string>();
             DispatcherTimer timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
 
-            solveHandler = (sender, args) =>
+            initialize = () =>
             {
-                string solution = this.captchText.Text;
+                this.panel.Visibility = Visibility.Visible;
+                this.captchaImage.Source = this.ApplyImage(captcha);
+                this.solve.Click += solveHandler;
+                this.solve.IsEnabled = false;
+                this.reload.Click += reloadHandler;
+                this.reload.IsEnabled = true;
+                this.cancel.Click += cancelHandler;
+                this.cancel.IsEnabled = true;
+
+                timer.Tick += timerHandler;
+                timer.Start();
+            };
+
+            dispose = () =>
+            {
+                this.panel.Visibility = Visibility.Collapsed;
 
                 timer.Tick -= timerHandler;
                 timer.Stop();
 
                 this.solve.Click -= solveHandler;
                 this.reload.Click -= reloadHandler;
+                this.cancel.Click -= cancelHandler;
                 this.captchaImage.Source = null;
                 this.captchText.Clear();
+            };
 
-                completion.SetResult(solution);
+            solveHandler = (sender, args) =>
+            {
+                string solution = this.captchText.Text;
+
+                dispose.Invoke();
+                completion.SetResult(solution.Trim());
             };
 
             reloadHandler = async (sender, args) =>
             {
-                await captcha.Reload();
-                this.captchaImage.Source = this.ApplyImage(captcha);
+                this.reload.IsEnabled = false;
+                this.cancel.IsEnabled = false;
+                this.solve.IsEnabled = this.CanBeSolved();
+
+                try
+                {
+                    await captcha.Reload();
+                    this.captchaImage.Source = this.ApplyImage(captcha);
+                }
+                finally
+                {
+                    this.reload.IsEnabled = true;
+                    this.cancel.IsEnabled = true;
+                    this.solve.IsEnabled = this.CanBeSolved();
+                }
+            };
+
+            cancelHandler = (sender, args) =>
+            {
+                dispose.Invoke();
+                completion.SetResult(null);
             };
 
             timerHandler = (sender, args) =>
             {
                 if (captcha.Cancellation.IsCancellationRequested == true)
                 {
-                    timer.Tick -= timerHandler;
-                    timer.Stop();
-
-                    this.solve.Click -= solveHandler;
-                    this.reload.Click -= reloadHandler;
-                    this.captchaImage.Source = null;
-                    this.captchText.Clear();
-
+                    dispose.Invoke();
                     completion.TrySetCanceled(captcha.Cancellation);
                 }
             };
 
-            this.captchaImage.Source = this.ApplyImage(captcha);
-            this.solve.Click += solveHandler;
-            this.reload.Click += reloadHandler;
-
-            timer.Tick += timerHandler;
-            timer.Start();
-
+            initialize.Invoke();
             return completion.Task;
         }
 
@@ -90,6 +120,16 @@ namespace ine.Views
             }
 
             return image;
+        }
+
+        private void HandleTextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.solve.IsEnabled = this.CanBeSolved();
+        }
+
+        private bool CanBeSolved()
+        {
+            return this.reload.IsEnabled && String.IsNullOrWhiteSpace(this.captchText.Text) == false;
         }
     }
 }
